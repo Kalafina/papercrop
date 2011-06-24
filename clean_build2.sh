f5b54a53e433e9a93b3a434f91bfb43ee31a8a6e
taesoobear <taesoobear@gmail.com>
2011-06-24 01:17:43 +0000

    fedora support

---
+++ clean_build2.sh
@@ -0,0 +1,14 @@
+cd libharu-2.0.8
+chmod 700 ./configure
+./configure
+make
+cd ..
+cd build_linux
+rm CMakeCache.txt
+cmake ..
+make clean
+make
+cp paperCrop ..
+mkdir -p ~/.referencer
+mkdir -p ~/.referencer/plugins
+cp ../referencer_plugins/papercrop* ~/.referencer/plugins/
