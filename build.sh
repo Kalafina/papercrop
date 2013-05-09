d21e32c60c4acf294db918615799e5160b5c833e
AaronInUSA <AaronInUSA@gmail.com>
2013-05-09 22:31:09 +0000

    build.sh now configures poppler

--- build.sh
+++ build.sh
@@ -2,11 +2,16 @@
 cd libharu-2.0.8
 chmod 700 ./configure
 ./configure
-make
+make -j 4
 cd ..
+
+cd poppler
+./autogen.sh
+cd ..
+
 mkdir -p build_linux
 cd build_linux
 cmake ..
-make
+make -j 4
 cp paperCrop ..
 
